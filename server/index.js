cb9c609a24f1c8bb6c3acd9308acd2655b9f6753
spaswan141 <paswanshubham0309@outlook.com>
2022-09-01 09:58:46 +0530

    all done

--- server/index.js
+++ server/index.js
@@ -1,22 +1,29 @@
-require("dotenv").config()
-const express=require('express')
-const cors=require('cors')
+require("dotenv").config();
+const express = require("express");
+
 const connection = require("./db");
-const blogRouter=require('./routers/blog.router')
-const reviewRouter=require('./routers/review.router')
-const app = express()
-connection()
-app.use(express.json())
-app.use(cors())
+const blogRouter=require('./routes/blog')
+const reviewRouter=require('./routes/review')
+var cors = require('cors')
+const app = express();
 
+connection();
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
 
+app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin",["https://shubham-paswan-blog.netlify.app"],"*",);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods",'GET,PUT,DELETE,POST');
+    next();
+});
 app.use("/",blogRouter);
 app.use('/',reviewRouter)
-app.use("/",(req, res, next) =>{
-res.send("done")
-})
-const Port = process.env.Port || 8080;
-app.listen(Port, () => {
-  console.log(`listening on port http://localhost:${Port}`);
-});
+
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+
 
